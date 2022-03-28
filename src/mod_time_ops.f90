fb20f6886f5d2411be4a28bd720b5c67e6c1045f
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-03-28 12:08:21 +0200

    Added acc to the adapt_dt subroutine, plus corrected a mistake with aux4 and max_MU.

--- src/mod_time_ops.f90
+++ src/mod_time_ops.f90
@@ -18,14 +18,16 @@ module mod_time_ops
 
          integer(4)                       :: inode, ielem
          real(8)                          :: dt_conv, dt_diff
-         real(8)                          :: umag, aux1, aux2, aux3, aux4, L3, max_MU
+         real(8)                          :: umag, aux1, aux2, aux4, L3, max_MU
 
          call nvtxStartRange("Adapting dt")
          dt_conv = 100000000000000.0d0
          dt_diff = 100000000000000.0d0
          dt      = 100000000000000.0d0
+         !$acc parallel loop gang reduction(min:dt_conv,dt_diff,dt)
          do ielem = 1,nelem
             L3 = 0.0d0
+            !$acc loop vector reduction(max:L3)
             do inode = 1,nnode
                umag = sqrt(dot_product(u(connec(ielem,inode),:),u(connec(ielem,inode),:)))
                aux1 = umag+csound(connec(ielem,inode))
@@ -34,14 +36,17 @@ module mod_time_ops
             aux2 = cfl_conv*(helem(ielem)/dble(porder))/L3
             dt_conv = min(dt_conv,aux2)
             if(present(cfl_diff) .and. present(mu_fluid) .and. present(rho)) then
-               aux3 = 0.0d0
-            do inode = 1,nnode
-               max_MU = max(aux3,mu_fluid(connec(ielem,inode))/rho(connec(ielem,inode)))
+               max_MU = 0.0d0
+               !$acc loop vector reduction(max:max_MU)
+               do inode = 1,nnode
+                  max_MU = max(max_MU,mu_fluid(connec(ielem,inode))/rho(connec(ielem,inode)))
                end do
+               aux4 = cfl_diff*((helem(ielem)/dble(porder))**2)/max_MU
                dt_diff = min(dt_diff,aux4)
             end if
             dt = min(dt_conv,dt_diff)
          end do
+         !$acc end parallel loop
          call nvtxEndRange
 
       end subroutine adapt_dt_cfl
