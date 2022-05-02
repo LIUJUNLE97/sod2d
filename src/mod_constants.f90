8a30420a41d9f6fb713499cb2474d63421b36228
Âolehmkuh <oriol.lehmkuhl@bsc.es>
2022-05-02 10:36:53 +0200

    Now we can read paraview binaries

--- src/mod_constants.f90
+++ src/mod_constants.f90
@@ -23,8 +23,8 @@ module mod_constants
       !
       ! Flags
       !
-      integer(4), parameter :: flag_real_diff=0
-      integer(4), parameter :: flag_diff_suth=0
+      integer(4), parameter :: flag_real_diff=1
+      integer(4), parameter :: flag_diff_suth=1
       integer(4), parameter :: flag_rk_order=4
       integer(4), parameter :: flag_les=0
       integer(4), parameter :: flag_solver_type=1    ! 1 = Lumped, 2 = APINV, 3 = CG
--- src/mod_output.f90
+++ src/mod_output.f90
@@ -327,4 +327,131 @@ module mod_output
       
       end subroutine write_vtk_binary
 
+      subroutine read_vtk_binary(isPeriodic,istep,npoin,nelem,coord,connec, &
+                                 rho,u,pr,E,mu_fluid,mu_e,mu_sgs,nper,masSla)
+         implicit none
+      
+         integer(4), intent(in)                            :: isPeriodic, nper
+         integer(4), intent(in)                            :: istep, npoin, nelem
+         integer(4), intent(in)                            :: connec(nelem,nnode)
+         integer(4), intent(in), optional                  :: masSla(nper,2)
+         real(8)   , intent(in)                            :: coord(npoin,ndime)
+         real(8)   , intent(inout), dimension(npoin)       :: rho, pr, E, mu_fluid
+         real(8)   , intent(inout), dimension(npoin,ndime) :: u
+         real(8)   , intent(inout), dimension(nelem,ngaus) :: mu_e
+         real(8)   , intent(inout), dimension(nelem,ngaus) :: mu_sgs
+         integer(4)                                        :: i, iper, ivtk=99
+         integer(4)            , dimension(nelem,nnode+1)  :: cells
+         integer(4)            , dimension(nelem)          :: cellTypes
+         real(8)               , dimension(npoin,3)        :: points
+         character(500)                                    :: filename
+
+         write(1,*) " begining the reading of the vtk binary "
+         call flush(1)
+
+         !
+         ! Open file with ascii input
+         !
+         write(filename,'("vtkTstep_",i0,".vtk")') istep
+         open(unit=ivtk,file=filename,status='old',access='stream',convert='BIG_ENDIAN',action='read') ! Binary file access with stream
+         
+         !
+         ! Read header in ascii format
+         !
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         
+         !
+         ! Read points
+         !
+         call skip_line(ivtk)
+         do i = 1,npoin
+            read(ivtk) points(i,:)
+         end do
+         
+         !
+         ! Read cells
+         !
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,nelem
+            read(ivtk) cells(i,:)
+         end do
+         
+         !
+         ! Read cell types
+         !
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,nelem
+            read(ivtk) cellTypes(i)
+         end do
+         
+         !
+         ! Read point scalar data
+         !
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,npoin
+            read(ivtk) rho(i)
+         end do
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,npoin
+            read(ivtk) pr(i)
+         end do
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,npoin
+            read(ivtk) E(i)
+         end do
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,npoin
+            read(ivtk) mu_fluid(i)
+         end do
+         !
+         ! Write point vector data
+         !
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         call skip_line(ivtk)
+         do i = 1,npoin
+            read(ivtk) u(i,:)
+         end do
+
+         write(1,*) " end the reading of the vtk binary "
+         call flush(1)
+         
+         close(ivtk)
+      
+      end subroutine read_vtk_binary
+
+      subroutine skip_line(ivtk)
+         implicit none
+      
+         integer(4), intent(in)                            :: ivtk
+         character(1)                                      :: lf, aux
+
+
+         lf = achar(10)
+         do while(.true.)
+            read(ivtk) aux
+            if(aux .eq. lf) exit
+         end do
+      end subroutine skip_line
 end module mod_output
