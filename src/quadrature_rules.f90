3354786bb4b7b3fc771a92be785133785e8ab065
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-05-03 15:24:21 +0200

    Quadrature now performed by ijk product.

--- src/quadrature_rules.f90
+++ src/quadrature_rules.f90
@@ -165,148 +165,172 @@ module quadrature_rules
                    
                    implicit none
 
-                   integer(4), intent(in) :: atoIJK(ngaus)
-                   real(8), intent(out) :: xgp(ngaus,ndime), wgp(ngaus)
-                   real(8)              :: xi(porder+1), w0, w1, w2, w3
+                   integer(4), intent(in)  :: atoIJK(ngaus)
+                   real(8),    intent(out) :: xgp(ngaus,ndime), wgp(ngaus)
+                   integer(4)              :: inode, i, j, k, lorder(porder+1)
+                   real(8)                 :: xi(porder+1), w1d(porder+1)
 
                    call chebyshev_roots(xi)
+                   lorder(1) = 1
+                   lorder(porder+1) = porder+1
+                   do i = 3,porder+1
+                      lorder(i) = i-1
+                   end do
 
-                   if (ngaus == 64) then
-                      xgp( 1,1:3) = [xi(1) , xi(1) , xi(1)]
-                      xgp( 2,1:3) = [xi(4) , xi(1) , xi(1)]
-                      xgp( 3,1:3) = [xi(4) , xi(4) , xi(1)]
-                      xgp( 4,1:3) = [xi(1) , xi(4) , xi(1)]
-                      xgp( 5,1:3) = [xi(1) , xi(1) , xi(4)]
-                      xgp( 6,1:3) = [xi(4) , xi(1) , xi(4)]
-                      xgp( 7,1:3) = [xi(4) , xi(4) , xi(4)]
-                      xgp( 8,1:3) = [xi(1) , xi(4) , xi(4)]
-                      xgp( 9,1:3) = [xi(2) , xi(1) , xi(1)]
-                      xgp(10,1:3) = [xi(3) , xi(1) , xi(1)]
-                      xgp(11,1:3) = [xi(1) , xi(2) , xi(1)]
-                      xgp(12,1:3) = [xi(1) , xi(3) , xi(1)]
-                      xgp(13,1:3) = [xi(1) , xi(1) , xi(2)]
-                      xgp(14,1:3) = [xi(1) , xi(1) , xi(3)]
-                      xgp(15,1:3) = [xi(4) , xi(2) , xi(1)]
-                      xgp(16,1:3) = [xi(4) , xi(3) , xi(1)]
-                      xgp(17,1:3) = [xi(4) , xi(1) , xi(2)]
-                      xgp(18,1:3) = [xi(4) , xi(1) , xi(3)]
-                      xgp(19,1:3) = [xi(3) , xi(4) , xi(1)]
-                      xgp(20,1:3) = [xi(2) , xi(4) , xi(1)]
-                      xgp(21,1:3) = [xi(4) , xi(4) , xi(2)]
-                      xgp(22,1:3) = [xi(4) , xi(4) , xi(3)]
-                      xgp(23,1:3) = [xi(1) , xi(4) , xi(2)]
-                      xgp(24,1:3) = [xi(1) , xi(4) , xi(3)]
-                      xgp(25,1:3) = [xi(2) , xi(1) , xi(4)]
-                      xgp(26,1:3) = [xi(3) , xi(1) , xi(4)]
-                      xgp(27,1:3) = [xi(1) , xi(2) , xi(4)]
-                      xgp(28,1:3) = [xi(1) , xi(3) , xi(4)]
-                      xgp(29,1:3) = [xi(4) , xi(2) , xi(4)]
-                      xgp(30,1:3) = [xi(4) , xi(3) , xi(4)]
-                      xgp(31,1:3) = [xi(3) , xi(4) , xi(4)]
-                      xgp(32,1:3) = [xi(2) , xi(4) , xi(4)]
-                      xgp(33,1:3) = [xi(2) , xi(2) , xi(1)]
-                      xgp(34,1:3) = [xi(2) , xi(3) , xi(1)]
-                      xgp(35,1:3) = [xi(3) , xi(3) , xi(1)]
-                      xgp(36,1:3) = [xi(3) , xi(2) , xi(1)]
-                      xgp(37,1:3) = [xi(2) , xi(1) , xi(2)]
-                      xgp(38,1:3) = [xi(3) , xi(1) , xi(2)]
-                      xgp(39,1:3) = [xi(3) , xi(1) , xi(3)]
-                      xgp(40,1:3) = [xi(2) , xi(1) , xi(3)]
-                      xgp(41,1:3) = [xi(1) , xi(2) , xi(2)]
-                      xgp(42,1:3) = [xi(1) , xi(2) , xi(3)]
-                      xgp(43,1:3) = [xi(1) , xi(3) , xi(3)]
-                      xgp(44,1:3) = [xi(1) , xi(3) , xi(2)]
-                      xgp(45,1:3) = [xi(4) , xi(2) , xi(2)]
-                      xgp(46,1:3) = [xi(4) , xi(3) , xi(2)]
-                      xgp(47,1:3) = [xi(4) , xi(3) , xi(3)]
-                      xgp(48,1:3) = [xi(4) , xi(2) , xi(3)]
-                      xgp(49,1:3) = [xi(3) , xi(4) , xi(2)]
-                      xgp(50,1:3) = [xi(2) , xi(4) , xi(2)]
-                      xgp(51,1:3) = [xi(2) , xi(4) , xi(3)]
-                      xgp(52,1:3) = [xi(3) , xi(4) , xi(3)]
-                      xgp(53,1:3) = [xi(2) , xi(2) , xi(3)]
-                      xgp(54,1:3) = [xi(3) , xi(2) , xi(3)]
-                      xgp(55,1:3) = [xi(3) , xi(3) , xi(3)]
-                      xgp(56,1:3) = [xi(2) , xi(3) , xi(3)]
-                      xgp(57,1:3) = [xi(2) , xi(2) , xi(2)]
-                      xgp(58,1:3) = [xi(3) , xi(2) , xi(2)]
-                      xgp(59,1:3) = [xi(3) , xi(3) , xi(2)]
-                      xgp(60,1:3) = [xi(2) , xi(3) , xi(2)]
-                      xgp(61,1:3) = [xi(2) , xi(2) , xi(3)]
-                      xgp(62,1:3) = [xi(3) , xi(2) , xi(3)]
-                      xgp(63,1:3) = [xi(3) , xi(3) , xi(3)]
-                      xgp(64,1:3) = [xi(2) , xi(3) , xi(3)]
-
-                      w0 = 1.0d0/9.0d0
-                      w1 = 8.0d0/9.0d0
-                      w2 = 8.0d0/9.0d0
-                      w3 = 1.0d0/9.0d0
-
-                      wgp( 1) = w0*w0*w0
-                      wgp( 2) = w3*w0*w0
-                      wgp( 3) = w3*w3*w0
-                      wgp( 4) = w0*w3*w0
-                      wgp( 5) = w0*w0*w3
-                      wgp( 6) = w3*w0*w3
-                      wgp( 7) = w3*w3*w3
-                      wgp( 8) = w0*w3*w3
-                      wgp( 9) = w1*w0*w0
-                      wgp(10) = w2*w0*w0
-                      wgp(11) = w1*w1*w0
-                      wgp(12) = w1*w2*w0
-                      wgp(13) = w0*w0*w1
-                      wgp(14) = w0*w0*w2
-                      wgp(15) = w3*w1*w0
-                      wgp(16) = w3*w2*w0
-                      wgp(17) = w3*w0*w1
-                      wgp(18) = w3*w0*w2
-                      wgp(19) = w2*w3*w0
-                      wgp(20) = w1*w3*w0
-                      wgp(21) = w3*w3*w1
-                      wgp(22) = w3*w3*w2
-                      wgp(23) = w0*w3*w1
-                      wgp(24) = w0*w3*w2
-                      wgp(25) = w1*w0*w3
-                      wgp(26) = w2*w0*w3
-                      wgp(27) = w0*w1*w3
-                      wgp(28) = w0*w2*w3
-                      wgp(29) = w3*w1*w3
-                      wgp(30) = w3*w2*w3
-                      wgp(31) = w2*w3*w3
-                      wgp(32) = w1*w3*w3
-                      wgp(33) = w1*w1*w0
-                      wgp(34) = w1*w2*w0
-                      wgp(35) = w2*w2*w0
-                      wgp(36) = w2*w1*w0
-                      wgp(37) = w1*w0*w1
-                      wgp(38) = w2*w0*w1
-                      wgp(39) = w2*w0*w2
-                      wgp(40) = w1*w0*w2
-                      wgp(41) = w0*w1*w1
-                      wgp(42) = w0*w1*w2
-                      wgp(43) = w0*w2*w2
-                      wgp(44) = w0*w2*w1
-                      wgp(45) = w3*w1*w1
-                      wgp(46) = w3*w2*w1
-                      wgp(47) = w3*w2*w2
-                      wgp(48) = w3*w1*w2
-                      wgp(49) = w2*w3*w1
-                      wgp(50) = w1*w3*w1
-                      wgp(51) = w1*w3*w2
-                      wgp(52) = w2*w3*w2
-                      wgp(53) = w1*w1*w3
-                      wgp(54) = w2*w1*w3
-                      wgp(55) = w2*w2*w3
-                      wgp(56) = w1*w2*w3
-                      wgp(57) = w1*w1*w1
-                      wgp(58) = w2*w1*w1
-                      wgp(59) = w2*w2*w1
-                      wgp(60) = w1*w2*w1
-                      wgp(61) = w1*w1*w2
-                      wgp(62) = w2*w1*w2
-                      wgp(63) = w2*w2*w2
-                      wgp(64) = w1*w2*w2
+                   if (porder == 3) then
+                      w1d(1:porder+1) = [1.0d0/9.0d0, 8.0d0/9.0d00, 8.0d0/9.0d0, 1.0d0/9.0d0]
+                   else
+                      STOP(1)
                    end if
+
+                   inode = 0
+                   do k = 1,porder+1
+                      do i = 1,porder+1
+                         do j = 1,porder+1
+                            inode = inode + 1
+                            xgp(atoIJK(inode),1:3) = [xi(lorder(i)), xi(lorder(j)), xi(lorder(k))]
+                            wgp(atoIJK(inode)) = w1d(lorder(i))*w1d(lorder(j))*w1d(lorder(k))
+                         end do
+                      end do
+                   end do
+
+                   ! TODO: remove when confirmed above method
+                   !!if (ngaus == 64) then
+                   !!   xgp( 1,1:3) = [xi(1) , xi(1) , xi(1)]
+                   !!   xgp( 2,1:3) = [xi(4) , xi(1) , xi(1)]
+                   !!   xgp( 3,1:3) = [xi(4) , xi(4) , xi(1)]
+                   !!   xgp( 4,1:3) = [xi(1) , xi(4) , xi(1)]
+                   !!   xgp( 5,1:3) = [xi(1) , xi(1) , xi(4)]
+                   !!   xgp( 6,1:3) = [xi(4) , xi(1) , xi(4)]
+                   !!   xgp( 7,1:3) = [xi(4) , xi(4) , xi(4)]
+                   !!   xgp( 8,1:3) = [xi(1) , xi(4) , xi(4)]
+                   !!   xgp( 9,1:3) = [xi(2) , xi(1) , xi(1)]
+                   !!   xgp(10,1:3) = [xi(3) , xi(1) , xi(1)]
+                   !!   xgp(11,1:3) = [xi(1) , xi(2) , xi(1)]
+                   !!   xgp(12,1:3) = [xi(1) , xi(3) , xi(1)]
+                   !!   xgp(13,1:3) = [xi(1) , xi(1) , xi(2)]
+                   !!   xgp(14,1:3) = [xi(1) , xi(1) , xi(3)]
+                   !!   xgp(15,1:3) = [xi(4) , xi(2) , xi(1)]
+                   !!   xgp(16,1:3) = [xi(4) , xi(3) , xi(1)]
+                   !!   xgp(17,1:3) = [xi(4) , xi(1) , xi(2)]
+                   !!   xgp(18,1:3) = [xi(4) , xi(1) , xi(3)]
+                   !!   xgp(19,1:3) = [xi(3) , xi(4) , xi(1)]
+                   !!   xgp(20,1:3) = [xi(2) , xi(4) , xi(1)]
+                   !!   xgp(21,1:3) = [xi(4) , xi(4) , xi(2)]
+                   !!   xgp(22,1:3) = [xi(4) , xi(4) , xi(3)]
+                   !!   xgp(23,1:3) = [xi(1) , xi(4) , xi(2)]
+                   !!   xgp(24,1:3) = [xi(1) , xi(4) , xi(3)]
+                   !!   xgp(25,1:3) = [xi(2) , xi(1) , xi(4)]
+                   !!   xgp(26,1:3) = [xi(3) , xi(1) , xi(4)]
+                   !!   xgp(27,1:3) = [xi(1) , xi(2) , xi(4)]
+                   !!   xgp(28,1:3) = [xi(1) , xi(3) , xi(4)]
+                   !!   xgp(29,1:3) = [xi(4) , xi(2) , xi(4)]
+                   !!   xgp(30,1:3) = [xi(4) , xi(3) , xi(4)]
+                   !!   xgp(31,1:3) = [xi(3) , xi(4) , xi(4)]
+                   !!   xgp(32,1:3) = [xi(2) , xi(4) , xi(4)]
+                   !!   xgp(33,1:3) = [xi(2) , xi(2) , xi(1)]
+                   !!   xgp(34,1:3) = [xi(2) , xi(3) , xi(1)]
+                   !!   xgp(35,1:3) = [xi(3) , xi(3) , xi(1)]
+                   !!   xgp(36,1:3) = [xi(3) , xi(2) , xi(1)]
+                   !!   xgp(37,1:3) = [xi(2) , xi(1) , xi(2)]
+                   !!   xgp(38,1:3) = [xi(3) , xi(1) , xi(2)]
+                   !!   xgp(39,1:3) = [xi(3) , xi(1) , xi(3)]
+                   !!   xgp(40,1:3) = [xi(2) , xi(1) , xi(3)]
+                   !!   xgp(41,1:3) = [xi(1) , xi(2) , xi(2)]
+                   !!   xgp(42,1:3) = [xi(1) , xi(2) , xi(3)]
+                   !!   xgp(43,1:3) = [xi(1) , xi(3) , xi(3)]
+                   !!   xgp(44,1:3) = [xi(1) , xi(3) , xi(2)]
+                   !!   xgp(45,1:3) = [xi(4) , xi(2) , xi(2)]
+                   !!   xgp(46,1:3) = [xi(4) , xi(3) , xi(2)]
+                   !!   xgp(47,1:3) = [xi(4) , xi(3) , xi(3)]
+                   !!   xgp(48,1:3) = [xi(4) , xi(2) , xi(3)]
+                   !!   xgp(49,1:3) = [xi(3) , xi(4) , xi(2)]
+                   !!   xgp(50,1:3) = [xi(2) , xi(4) , xi(2)]
+                   !!   xgp(51,1:3) = [xi(2) , xi(4) , xi(3)]
+                   !!   xgp(52,1:3) = [xi(3) , xi(4) , xi(3)]
+                   !!   xgp(53,1:3) = [xi(2) , xi(2) , xi(3)]
+                   !!   xgp(54,1:3) = [xi(3) , xi(2) , xi(3)]
+                   !!   xgp(55,1:3) = [xi(3) , xi(3) , xi(3)]
+                   !!   xgp(56,1:3) = [xi(2) , xi(3) , xi(3)]
+                   !!   xgp(57,1:3) = [xi(2) , xi(2) , xi(2)]
+                   !!   xgp(58,1:3) = [xi(3) , xi(2) , xi(2)]
+                   !!   xgp(59,1:3) = [xi(3) , xi(3) , xi(2)]
+                   !!   xgp(60,1:3) = [xi(2) , xi(3) , xi(2)]
+                   !!   xgp(61,1:3) = [xi(2) , xi(2) , xi(3)]
+                   !!   xgp(62,1:3) = [xi(3) , xi(2) , xi(3)]
+                   !!   xgp(63,1:3) = [xi(3) , xi(3) , xi(3)]
+                   !!   xgp(64,1:3) = [xi(2) , xi(3) , xi(3)]
+
+                   !!   w0 = 1.0d0/9.0d0
+                   !!   w1 = 8.0d0/9.0d0
+                   !!   w2 = 8.0d0/9.0d0
+                   !!   w3 = 1.0d0/9.0d0
+
+                   !!   wgp( 1) = w0*w0*w0
+                   !!   wgp( 2) = w3*w0*w0
+                   !!   wgp( 3) = w3*w3*w0
+                   !!   wgp( 4) = w0*w3*w0
+                   !!   wgp( 5) = w0*w0*w3
+                   !!   wgp( 6) = w3*w0*w3
+                   !!   wgp( 7) = w3*w3*w3
+                   !!   wgp( 8) = w0*w3*w3
+                   !!   wgp( 9) = w1*w0*w0
+                   !!   wgp(10) = w2*w0*w0
+                   !!   wgp(11) = w1*w1*w0
+                   !!   wgp(12) = w1*w2*w0
+                   !!   wgp(13) = w0*w0*w1
+                   !!   wgp(14) = w0*w0*w2
+                   !!   wgp(15) = w3*w1*w0
+                   !!   wgp(16) = w3*w2*w0
+                   !!   wgp(17) = w3*w0*w1
+                   !!   wgp(18) = w3*w0*w2
+                   !!   wgp(19) = w2*w3*w0
+                   !!   wgp(20) = w1*w3*w0
+                   !!   wgp(21) = w3*w3*w1
+                   !!   wgp(22) = w3*w3*w2
+                   !!   wgp(23) = w0*w3*w1
+                   !!   wgp(24) = w0*w3*w2
+                   !!   wgp(25) = w1*w0*w3
+                   !!   wgp(26) = w2*w0*w3
+                   !!   wgp(27) = w0*w1*w3
+                   !!   wgp(28) = w0*w2*w3
+                   !!   wgp(29) = w3*w1*w3
+                   !!   wgp(30) = w3*w2*w3
+                   !!   wgp(31) = w2*w3*w3
+                   !!   wgp(32) = w1*w3*w3
+                   !!   wgp(33) = w1*w1*w0
+                   !!   wgp(34) = w1*w2*w0
+                   !!   wgp(35) = w2*w2*w0
+                   !!   wgp(36) = w2*w1*w0
+                   !!   wgp(37) = w1*w0*w1
+                   !!   wgp(38) = w2*w0*w1
+                   !!   wgp(39) = w2*w0*w2
+                   !!   wgp(40) = w1*w0*w2
+                   !!   wgp(41) = w0*w1*w1
+                   !!   wgp(42) = w0*w1*w2
+                   !!   wgp(43) = w0*w2*w2
+                   !!   wgp(44) = w0*w2*w1
+                   !!   wgp(45) = w3*w1*w1
+                   !!   wgp(46) = w3*w2*w1
+                   !!   wgp(47) = w3*w2*w2
+                   !!   wgp(48) = w3*w1*w2
+                   !!   wgp(49) = w2*w3*w1
+                   !!   wgp(50) = w1*w3*w1
+                   !!   wgp(51) = w1*w3*w2
+                   !!   wgp(52) = w2*w3*w2
+                   !!   wgp(53) = w1*w1*w3
+                   !!   wgp(54) = w2*w1*w3
+                   !!   wgp(55) = w2*w2*w3
+                   !!   wgp(56) = w1*w2*w3
+                   !!   wgp(57) = w1*w1*w1
+                   !!   wgp(58) = w2*w1*w1
+                   !!   wgp(59) = w2*w2*w1
+                   !!   wgp(60) = w1*w2*w1
+                   !!   wgp(61) = w1*w1*w2
+                   !!   wgp(62) = w2*w1*w2
+                   !!   wgp(63) = w2*w2*w2
+                   !!   wgp(64) = w1*w2*w2
+                   !!end if
 
                 end subroutine chebyshev_hex
 
