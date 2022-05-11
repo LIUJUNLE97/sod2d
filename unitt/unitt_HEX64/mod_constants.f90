92c69727e0ff041efe1d63c6992e02bf470bd6d7
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-05-11 12:09:45 +0200

    Added a unit test for the spectral HEX64.

---
+++ unitt/unitt_HEX64/mod_constants.f90
@@ -0,0 +1,47 @@
+module mod_constants
+
+      implicit none
+
+      !
+      ! Dimensions
+      !
+      integer(4), parameter :: ndime=3
+      
+      !
+      ! Element characteristics
+      !
+      integer(4), parameter :: nnode=64
+      integer(4), parameter :: porder=3
+      integer(4), parameter :: npbou=16
+      integer(4), parameter :: ngaus=64
+
+      !
+      ! Flags
+      !
+      integer(4), parameter :: flag_real_diff=1
+      integer(4), parameter :: flag_diff_suth=1
+      integer(4), parameter :: flag_rk_order=4
+      integer(4), parameter :: flag_les=0
+      integer(4), parameter :: flag_solver_type=1    ! 1 = Lumped, 2 = APINV, 3 = CG
+      integer(4), parameter :: flag_spectralElem=1  ! 0 for Lagrange type, 1 for Chebyshev type
+
+      !
+      ! Solver params
+      !
+      integer(4), parameter :: maxIter=3
+      real(8)   , parameter :: tol=0.00001d0
+
+      !
+      ! Other constants
+      !
+      real(8), parameter :: v_pi = 2.0d0*asin(1.0d0) ! Value of Pi
+      real(8), parameter :: ce = 1.0d0   
+      real(8), parameter :: cmax = 0.05d0 ! for FEM 0.5 for SEM 0.05/p
+      real(8), parameter :: cglob = 1.0d0
+      real(8), parameter :: c_rho = 1.0d0
+      real(8), parameter :: c_ener = 1.0d0
+      real(8), parameter :: c_sgs = 0.07d0
+
+      real(8) :: flag_mu_factor=1.0d0
+
+end module mod_constants
---
+++ unitt/unitt_HEX64/unitt_hex64_test.f90
@@ -0,0 +1,68 @@
+program unitt_hex64_test
+
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   ! Tests how well the element type HEX27 is implemented through     !
+   ! geometrical tests over the reference element.                    !
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+   use mod_constants     ! Local version of mod_constants
+   use elem_hex          ! Module with all basic element definitions
+   use mod_maths         ! Module with all mathematical functions
+   use quadrature_rules  ! Module with all quadrature rules
+   use jacobian_oper     ! Module with all jacobian operators
+
+   implicit none
+
+   integer(4) :: idime, inode, igaus
+   integer(4) :: jdime, jnode, jgaus
+   integer(4) :: atoIJK(ngaus), listHEX08(27,8)
+   real(8)    :: s, t, z, aux
+   real(8)    :: elcod(nnode,ndime)
+   real(8)    :: xgp(ngaus,ndime), wgp(ngaus)
+   real(8)    :: Nl(nnode), dNl(ndime,nnode)
+   real(8)    :: N(ngaus,nnode), dN(ndime,nnode,ngaus)
+   logical    :: flag_pass
+
+   !
+   ! Initialize check
+   !
+   flag_pass = .true. ! Will flip to false if a test fails
+
+   !
+   ! Element geometry: comes from the chebyshev_hex quadrature rule
+   !
+   call hex64(1.0d0,1.0d0,1.0d0,atoIJK,listHEX08)
+   call chebyshev_hex(atoIJK,xgp,wgp)
+
+   !
+   ! Compute N_a(xi_b); check whether it is 1 only when a == b
+   !
+   outer: do igaus = 1,ngaus
+      s = xgp(igaus,1)
+      t = xgp(igaus,2)
+      z = xgp(igaus,3)
+      call hex64(s,t,z,atoIJK,listHEX08,N(igaus,:),dN(:,:,igaus),Nl,dNl)
+      do jgaus = 1,ngaus
+         aux = N(igaus,jgaus)
+         if (jgaus == igaus .and. aux .ne. 1.0d0) then
+            flag_pass = .false.
+            exit outer
+         else if (jgaus .ne. igaus .and. aux .ne. 0.0d0) then
+            flag_pass = .false.
+            exit outer
+         end if
+      end do
+   end do outer
+
+   !
+   ! Check if test failed
+   !
+   if (flag_pass) then
+      write(*,*) 'HEX64: passed'
+      STOP
+   else
+      write(*,*) 'HEX64: failed'
+      ERROR STOP
+   end if
+
+end program unitt_hex64_test
