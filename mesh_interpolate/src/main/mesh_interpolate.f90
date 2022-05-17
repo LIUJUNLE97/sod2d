5825054c0f065b89ed59e360d78903aa91d21491
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-05-17 12:39:12 +0200

    Now it reads dims for both meshes.

--- mesh_interpolate/src/main/mesh_interpolate.f90
+++ mesh_interpolate/src/main/mesh_interpolate.f90
@@ -7,10 +7,19 @@ program mesh_interpolate
 
     integer(4)     :: lin_npoin, lin_nelem, lin_nboun
     integer(4)     :: npoin, nelem, nboun
+    character(500) :: lin_file_path, lin_file_name
     character(500) :: file_path, file_name
 
+    ! Define case name
+    write(lin_file_name,*) "cube"
+    file_name = lin_file_name
+
     ! Read the linear mesh
+    write(lin_file_path,*) "./mesh_lin/"
+    call read_dims(lin_file_path,lin_file_name,lin_npoin,lin_nelem,lin_nboun)
+
+    ! Read the high-order mesh
     write(file_path,*) "./mesh/"
-    write(file_name,*) "cube"
-    call read_dims(file_path,file_name,lin_npoin,lin_nelem,nboun)
+    call read_dims(file_path,file_name,npoin,nelem,nboun)
+
 end program mesh_interpolate
