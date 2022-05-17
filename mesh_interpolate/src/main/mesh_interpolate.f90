8fae12a31c97c17a008e38a9eaf598e719c818c8
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-05-17 12:34:39 +0200

    Now it can read the linear mesh.

--- mesh_interpolate/src/main/mesh_interpolate.f90
+++ mesh_interpolate/src/main/mesh_interpolate.f90
@@ -1,7 +1,16 @@
 program mesh_interpolate
 
+    use mod_constants
     use mesh_reader
-    implicit none
-    
 
+    implicit none
+
+    integer(4)     :: lin_npoin, lin_nelem, lin_nboun
+    integer(4)     :: npoin, nelem, nboun
+    character(500) :: file_path, file_name
+
+    ! Read the linear mesh
+    write(file_path,*) "./mesh/"
+    write(file_name,*) "cube"
+    call read_dims(file_path,file_name,lin_npoin,lin_nelem,nboun)
 end program mesh_interpolate
