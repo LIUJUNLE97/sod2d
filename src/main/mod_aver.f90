02772efbdd403bc889d79f46318c5060a7bc033f
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-05-26 17:35:33 +0200

    Added avrhopr. Commented out so it willl compile the dummy.

--- src/main/mod_aver.f90
+++ src/main/mod_aver.f90
@@ -2,14 +2,15 @@ module mod_aver
 	contains
 	subroutine  favre_average()
 		implicit none
-		! Compute accumulated time
-		avtim = avtim+dt
-		! Compute accumulated tally for density times current dt and other variables times density times current dt
-		do ipoin = 1,npoin_w
-			avden(lpoin_w(ipoin)) = avden(lpoin_w(ipoin)) + rho(lpoin_w(ipoin))*dt ! Sum(rho*dt)
-			avrhou(lpoin_w(ipoin),1) = avrhou(lpoin_w(ipoin),1) + rho(lpoin_w(ipoin))*u(lpoin_w(ipoin),1)*dt ! Sum(rho*u1*dt)
-			avrhou(lpoin_w(ipoin),2) = avrhou(lpoin_w(ipoin),2) + rho(lpoin_w(ipoin))*u(lpoin_w(ipoin),2)*dt ! Sum(rho*u2*dt)
-			avrhou(lpoin_w(ipoin),3) = avrhou(lpoin_w(ipoin),3) + rho(lpoin_w(ipoin))*u(lpoin_w(ipoin),3)*dt ! Sum(rho*u3*dt)
-		end do
+		!! Compute accumulated time
+		!avtim = avtim+dt
+		!! Compute accumulated tally for density times current dt and other variables times density times current dt
+		!do ipoin = 1,npoin_w
+		!	avden(lpoin_w(ipoin)) = avden(lpoin_w(ipoin)) + rho(lpoin_w(ipoin))*dt ! Sum(rho*dt)
+		!	avrhou(lpoin_w(ipoin),1) = avrhou(lpoin_w(ipoin),1) + rho(lpoin_w(ipoin))*u(lpoin_w(ipoin),1)*dt ! Sum(rho*u1*dt)
+		!	avrhou(lpoin_w(ipoin),2) = avrhou(lpoin_w(ipoin),2) + rho(lpoin_w(ipoin))*u(lpoin_w(ipoin),2)*dt ! Sum(rho*u2*dt)
+		!	avrhou(lpoin_w(ipoin),3) = avrhou(lpoin_w(ipoin),3) + rho(lpoin_w(ipoin))*u(lpoin_w(ipoin),3)*dt ! Sum(rho*u3*dt)
+		!	avrhopr(lpoin_w(ipoin)) = avrhopr(lpoin_w(ipoin)) + rho(lpoin_w(ipoin))*pr(lpoin_w(ipoin))*dt ! Sum(rho*pr*dt)
+		!end do
 	end subroutine favre_average
 end module mod_aver
