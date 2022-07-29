623965a5623dd814173114ae76da92f7cf783501
Âolehmkuh <oriol.lehmkuhl@bsc.es>
2022-07-29 12:46:59 +0200

    more roboust flow control for the themal channel flow

--- src/main/classes/ThermalChannelFlowSolver.f90
+++ src/main/classes/ThermalChannelFlowSolver.f90
@@ -182,11 +182,10 @@ contains
          area = this%delta*2.0*v_pi*this%delta*v_pi
          tw = 0.5_rp*((Ftau(codeH,1)/area)+(Ftau(codeC,1)/area))
          if(tw .le. this%tauw) then 
-            tw = tw*1.01_rp
+            source_term(1) = source_term(1)*1.01_rp
          else  
-            tw = tw*0.99_rp    
+            source_term(1) = source_term(1)*0.99_rp
          end if
-        source_term(1) = tw/this%delta
         source_term(2) = 0.00_rp
         source_term(3) = 0.00_rp
       end if
