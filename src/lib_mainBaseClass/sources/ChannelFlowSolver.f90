24b0e745851e7f558e733728d9fe3889a8351501
Âolehmkuh <oriol.lehmkuhl@bsc.es>
2022-10-11 10:07:29 +0200

    Small change in ce

--- src/lib_mainBaseClass/sources/ChannelFlowSolver.f90
+++ src/lib_mainBaseClass/sources/ChannelFlowSolver.f90
@@ -83,8 +83,8 @@ contains
 !      this%load_step = 400001
 
       this%nstep = 9000000 
-      this%cfl_conv = 1.0_rp
-      this%cfl_diff = 1.0_rp
+      this%cfl_conv = 0.75_rp
+      this%cfl_diff = 0.75_rp
       this%nsave  = 1  ! First step to save, TODO: input
       this%nsave2 = 1   ! First step to save, TODO: input
       this%nsaveAVG = 1
