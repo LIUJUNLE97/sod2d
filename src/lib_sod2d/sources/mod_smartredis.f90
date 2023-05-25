8d9f223ea1666bc3e06f0d07de7365bb227825f8
Bernat Font <bernat.font@bsc.es>
2023-05-25 20:32:54 +0200

    Initial implementation of the gathered write of witness points into DB.

--- src/lib_sod2d/sources/mod_smartredis.f90
+++ src/lib_sod2d/sources/mod_smartredis.f90
@@ -33,20 +33,30 @@ module mod_smartredis
    end subroutine end_smartredis
 
    ! Write witness points state into DB
-   subroutine write_witness_state(client, state_size, state, key_name)
+
+   subroutine write_witness_state(client, state_local_size, state_global_size, state_local, state_sizes, displs, key)
       type(client_type), intent(inout) :: client
-      integer(4), intent(in) :: state_size         ! witness points size
-      real(rp), intent(in) :: state(state_size)    ! witness points state values
-      character(len=*), intent(in) :: key_name     ! array name to write to database
-      character(len=8) :: key_mpi_prefix           ! mpi rank prefix
-      character(len=32) :: key                     ! name + mpi rank prefix
-      integer :: return_code
+      integer, intent(in) :: state_local_size               ! local number of witness points
+      integer, intent(in) :: state_global_size              ! total number of witness points
+      real(rp), intent(in) :: state_local(state_local_size) ! local witness points state values
+      integer, intent(in) :: state_sizes(mpi_size)          ! array containing the number of witness points per each rank
+      integer, intent(in) :: displs(mpi_size)               ! array containing displacements for the number of witness points per each rank
+      character(len=*), intent(in) :: key                   ! array name to write to database
+      integer :: error
+      real(rp) :: state_global(state_global_size)
 
-      write(key_mpi_prefix,'(I6.6,A)') mpi_rank, '_'
-      key = key_mpi_prefix // trim(key_name)
+      ! gather the local states into a global state
+      call mpi_gatherv( &
+         state_local, state_local_size, mpi_datatype_real,     & ! everyone sends state_local data
+         state_global, state_sizes, displs, mpi_datatype_real, & ! root receives it into state_global
+         0, mpi_comm_world, error                              & ! rank 0 is root
+      )
 
-      return_code = client%put_tensor(key, state, shape(state))
-      if (return_code /= SRNoError) stop 'Error during SmartRedis state writting.'
+      ! write global state into DB
+      if (mpi_rank .eq. 0) then
+         error = client%put_tensor(key, state_global, shape(state_global))
+         if (error /= SRNoError) stop 'Error during SmartRedis state writting.'
+      end if
    end subroutine write_witness_state
 
    ! Read actions from DB
@@ -60,18 +70,17 @@ module mod_smartredis
       character(len=8) :: key_mpi_prefix               ! mpi rank prefix
       character(len=32) :: key                         ! name + mpi rank prefix
       logical(kind=c_bool) :: exists                   ! receives whether the tensor exists
-      logical :: found
-      integer :: return_code
+      integer :: found, error
 
       write(key_mpi_prefix,'(I6.6,A)') mpi_rank, '_'
       key = trim(key_mpi_prefix) // trim(key_name)
 
       found = client%poll_tensor(key, interval, tries, exists)
-      if (.not. found) stop 'Error in SmartRedis actions reading. Actions array not found.'
-      return_code = client%unpack_tensor(key, actions, shape(actions))
-      if (return_code /= SRNoError) stop 'Error during SmartRedis actions reading.'
-      return_code = client%delete_tensor(key)
-      if (return_code /= SRNoError) stop 'Error in SmartRedis actions reading. Tensor could not be deleted.'
+      if (found /= 0) stop 'Error in SmartRedis actions reading. Actions array not found.'
+      error = client%unpack_tensor(key, actions, shape(actions))
+      if (error /= SRNoError) stop 'Error during SmartRedis actions reading.'
+      error = client%delete_tensor(key)
+      if (error /= SRNoError) stop 'Error in SmartRedis actions reading. Tensor could not be deleted.'
    end subroutine read_actions
 
 #endif
