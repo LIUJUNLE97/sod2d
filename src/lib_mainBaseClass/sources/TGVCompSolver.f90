f42fac49c71e055dd68bcff650517320e0701fb1
Âolehmkuh <oriol.lehmkuhl@bsc.es>
2023-08-22 15:14:07 +0200

    TGC Comp now is integrated

--- src/lib_mainBaseClass/sources/TGVCompSolver.f90
+++ src/lib_mainBaseClass/sources/TGVCompSolver.f90
@@ -32,7 +32,7 @@ module TGVCompSolver_mod
 
    type, public, extends(CFDSolverPeriodic) :: TGVCompSolver
 
-      real(rp) , public  ::  rho0,Re,to, po
+      real(rp) , public  ::  Ma, V0,L,T0,P0,Re,rho0
 
    contains
       procedure, public :: initializeParameters  => TGVCompSolver_initializeParameters
@@ -52,6 +52,7 @@ contains
 
       this%doGlobalAnalysis = .true.
 
+      this%maxPhysTime = 20.0_rp
       this%final_istep = 10000 
       this%cfl_conv = 0.95_rp
       this%cfl_diff = 0.95_rp
@@ -63,29 +64,29 @@ contains
       this%restartFile_to_load = 1 !1 or 2
       this%continue_oldLogs = .false.
       this%save_restartFile_first = 1
-      this%save_restartFile_step = 50000
+      this%save_restartFile_step = 500
 
       this%save_resultsFile_first = 1
-      this%save_resultsFile_step = 50000
+      this%save_resultsFile_step = 500
 
-      !this%save_avgResultsFile_first = 1
-      !this%save_avgResultsFile_step = 50000
-
-      this%Cp = 1004.0_rp
+      this%Ma = 1.25_rp
+      this%L = 1.0_rp
+      this%V0 = 1.0_rp
       this%Prt = 0.71_rp
-      this%to  = 1.0_rp
+      this%T0  = 1.0_rp
       this%Re = 1600.0_rp
       this%gamma_gas = 1.40_rp
-
+      
       this%Rgas = 1.0_rp*1.0_rp/(1.4_rp*1.0_rp*1.25_rp*1.25_rp)
-      this%rho0 = (1.0_rp/(1.4_rp*1.25*1.25))/(this%Rgas*this%to)
+      this%Cp = this%gamma_gas*this%Rgas/(this%gamma_gas-1.0_rp)
+      this%rho0 = (1.0_rp/(1.4_rp*1.25*1.25))/(this%Rgas*this%T0)
 
       mul    = (this%rho0*1.0_rp*1.0_rp)/this%Re
-      this%po = this%rho0*this%Rgas*this%to
-      mur = 0.000001458_rp*(this%to**1.50_rp)/(this%to+110.40_rp)
+      this%P0 = 1.0_rp/(this%Ma*this%Ma*1.4_rp)
+      mur = 0.000001458_rp*(this%T0**1.50_rp)/(this%T0+110.40_rp)
       flag_mu_factor = mul/mur
 
-      nscbc_p_inf = this%po
+      nscbc_p_inf = this%P0
       nscbc_Rgas_inf = this%Rgas
       nscbc_gamma_inf = this%gamma_gas
 
@@ -93,15 +94,26 @@ contains
 
    subroutine TGVCompSolver_evalInitialConditions(this)
       class(TGVCompSolver), intent(inout) :: this
-      integer(8) :: matGidSrlOrdered(numNodesRankPar,2)
+      real(4) :: x,y,z
       integer(4) :: iNodeL
-      character(512) :: initialField_filePath
 
-      call order_matrix_globalIdSrl(numNodesRankPar,globalIdSrl,matGidSrlOrdered)
-      write(initialField_filePath,*) ""
-      call read_veloc_from_file_Par(numElemsRankPar,numNodesRankPar,totalNumNodesSrl,initialField_filePath,u(:,:,2),connecParOrig,Ngp_l,matGidSrlOrdered)
-      call read_densi_from_file_Par(numElemsRankPar,numNodesRankPar,totalNumNodesSrl,initialField_filePath,rho(:,2),connecParOrig,Ngp_l,matGidSrlOrdered)
-      call read_press_from_file_Par(numElemsRankPar,numNodesRankPar,totalNumNodesSrl,initialField_filePath,pr(:,2),connecParOrig,Ngp_l,matGidSrlOrdered)
+      if(mpi_rank.eq.0) write(*,*) "--| TGV - Setting Initial Conditions..."
+
+
+      !$acc parallel loop
+      do iNodeL=1,numNodesRankPar
+         x = coordPar(iNodeL,1)
+         y = coordPar(iNodeL,2)
+         z = coordPar(iNodeL,3)
+
+         u(iNodeL,1,2) =  this%V0*sin(x/(this%L))*cos(y/(this%L))*cos(z/(this%L))
+         u(iNodeL,2,2) = -this%V0*cos(x/(this%L))*sin(y/(this%L))*cos(z/(this%L))
+         u(iNodeL,3,2) = 0.0
+
+         pr(iNodeL,2)  = this%P0+((1.0_rp*this%V0*this%V0)/(16.0_rp))*(cos(2.0_rp*x/this%L)+cos(2.0_rp*y/this%L))*(cos(2.0_rp*z/this%L)+2.0_rp)
+         rho(iNodeL,2) = pr(iNodeL,2)/this%Rgas/this%T0
+      end do
+      !$acc end parallel loop
 
       !$acc parallel loop
       do iNodeL = 1,numNodesRankPar
@@ -110,6 +122,12 @@ contains
          E(iNodeL,2) = rho(iNodeL,2)*(0.5_rp*dot_product(u(iNodeL,:,2),u(iNodeL,:,2))+e_int(iNodeL,2))
          q(iNodeL,1:ndime,2) = rho(iNodeL,2)*u(iNodeL,1:ndime,2)
          csound(iNodeL) = sqrt(this%gamma_gas*pr(iNodeL,2)/rho(iNodeL,2))
+         eta(iNodeL,2) = (rho(iNodeL,2)/(this%gamma_gas-1.0_rp))*log(pr(iNodeL,2)/(rho(iNodeL,2)**this%gamma_gas))
+
+         q(iNodeL,1:ndime,3) = q(iNodeL,1:ndime,2)
+         rho(iNodeL,3) = rho(iNodeL,2)
+         E(iNodeL,3) =  E(iNodeL,2)
+         eta(iNodeL,3) =  eta(iNodeL,2)
       end do
       !$acc end parallel loop
 
