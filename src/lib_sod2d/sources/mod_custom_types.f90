3a6d0fd99543249b46adf22e0691744b9fc33f66
Jordi Muela <jordi.muela@gmail.com>
2023-08-22 11:46:09 +0200

    cleaned mod_comms and mod_numerical_params updated mod_custom_types

--- src/lib_sod2d/sources/mod_custom_types.f90
+++ src/lib_sod2d/sources/mod_custom_types.f90
@@ -11,8 +11,72 @@ module mod_custom_types
       real(rp),dimension(:,:),pointer :: ptr
    end type ptr_array2d_rp
 
-   !type domainptr
-      !  type(ptr_array), pointer :: p
-   !end type domainptr
+!------------------------------------------------------------------------------------------------------
+
+   type vector_int4
+      integer(4),dimension(:), allocatable :: elems
+   end type vector_int4
+
+   type vector_int8
+      integer(8),dimension(:), allocatable :: elems
+   end type vector_int8
+
+   type matrix_int4
+      integer(4),dimension(:,:), allocatable :: elems
+   end type matrix_int4
+
+   type matrix_int8
+      integer(8),dimension(:,:), allocatable :: elems
+   end type matrix_int8
+
+   type vector_rp
+      real(rp),dimension(:), allocatable :: elems
+   end type vector_rp
+
+   type matrix_rp
+      real(rp),dimension(:,:), allocatable :: elems
+   end type matrix_rp
+
+   type vector_real8
+      real(8),dimension(:), allocatable :: elems
+   end type vector_real8
+
+   type matrix_real8
+      real(8),dimension(:,:), allocatable :: elems
+   end type matrix_real8
+
+   type jagged_vector_int4
+      type(vector_int4),dimension(:), allocatable :: vector
+   end type jagged_vector_int4
+
+   type jagged_matrix_int4
+      type(matrix_int4),dimension(:), allocatable :: matrix
+   end type jagged_matrix_int4
+
+   type jagged_vector_int8
+      type(vector_int8),dimension(:), allocatable :: vector
+   end type jagged_vector_int8
+
+   type jagged_matrix_int8
+      type(matrix_int8),dimension(:), allocatable :: matrix
+   end type jagged_matrix_int8
+
+   type jagged_vector_rp
+      type(vector_rp),dimension(:), allocatable :: vector
+   end type jagged_vector_rp
+
+   type jagged_matrix_rp
+      type(matrix_rp),dimension(:), allocatable :: matrix
+   end type jagged_matrix_rp
+
+   type jagged_vector_real8
+      type(vector_real8),dimension(:), allocatable :: vector
+   end type jagged_vector_real8
+
+   type jagged_matrix_real8
+      type(matrix_real8),dimension(:), allocatable :: matrix
+   end type jagged_matrix_real8
+
+!------------------------------------------------------------------------------------------------------
 
 end module mod_custom_types
