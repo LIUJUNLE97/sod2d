227bf9df53695db0fbb4e8e1f64c4fcc354df47d
Lucas Gasparino <lucas.gasparino@bsc.es>
2023-08-22 17:35:37 +0200

    Cleaanned quadrature_rules.

--- src/lib_sod2d/sources/quadrature_rules.f90
+++ src/lib_sod2d/sources/quadrature_rules.f90
@@ -1,9 +1,9 @@
 module quadrature_rules
 
-   use mod_constants
-   use mod_maths
+	use mod_constants
+	use mod_maths
 
-   contains
+	contains
 
 		!> @brief Computes the integral of a function over a QUA_XX
 		!> @details Closed rule quadrature for a function f(x) over a
@@ -15,13 +15,14 @@ module quadrature_rules
 		!> @param[out] xgp Quadrature points
 		!> @param[out] wgp Quadrature weights
 		subroutine GaussLobattoLegendre_qua(mporder,mnpbou,atoIJ,xgp,wgp)
+
 			implicit none
 			integer(4),intent(in) :: mporder,mnpbou,atoIJ(mnpbou)
 			real(rp),intent(out)  :: xgp(mnpbou,ndime-1),wgp(mnpbou)
 			integer(4)            :: inode,i,j,lorder(mporder+1)
 			real(rp)              :: xi(mporder+1),w1d(mporder+1)
 
-         call getGaussLobattoLegendre_weights_and_roots(mporder,w1d,xi)
+			call getGaussLobattoLegendre_weights_and_roots(mporder,w1d,xi)
 
 			lorder(1) = 1
 			lorder(2) = mporder+1
@@ -39,7 +40,7 @@ module quadrature_rules
 			end do
 		end subroutine GaussLobattoLegendre_qua
 
-      !> @brief Computes the integral of a function over a HEX_XX
+		!> @brief Computes the integral of a function over a HEX_XX
 		!> @details Closed rule quadrature for a function f(x) over a
 		!> HEX_XX element type. The integral is computed using the
 		!> given grid and weights associated with a particular element order.
@@ -47,78 +48,65 @@ module quadrature_rules
 		!> @param[in] atoIJK Node a to IJK rellationship
 		!> @param[out] xgp Quadrature points
 		!> @param[out] wgp Quadrature weights
-      subroutine GaussLobattoLegendre_hex(mporder,mngaus,atoIJK,xgp,wgp)
-         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-         ! Closed rule quadrature that uses the     !
-         ! Chebyshev grid+enddpoints as abcissas.   !
-         ! Weights are obtained by evaluating       !
-         ! w_j = int(l^n_i(xi_j),-1,1).             !
-         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-         implicit none
-         integer(4),intent(in) :: mporder,mngaus,atoIJK(mngaus)
-         real(rp),intent(out)  :: xgp(mngaus,ndime),wgp(mngaus)
-         integer(4)            :: inode,i,j,k,lorder(mporder+1)
-         real(rp)              :: xi(mporder+1),w1d(mporder+1),w0,w1,w2,w3
+		subroutine GaussLobattoLegendre_hex(mporder,mngaus,atoIJK,xgp,wgp)
 
-         call getGaussLobattoLegendre_weights_and_roots(mporder,w1d,xi)
+			implicit none
+			integer(4),intent(in) :: mporder,mngaus,atoIJK(mngaus)
+			real(rp),intent(out)  :: xgp(mngaus,ndime),wgp(mngaus)
+			integer(4)            :: inode,i,j,k,lorder(mporder+1)
+			real(rp)              :: xi(mporder+1),w1d(mporder+1),w0,w1,w2,w3
 
-         lorder(1) = 1
-         lorder(2) = mporder+1
-         do i = 3,mporder+1
-            lorder(i) = i-1
-         end do
+			call getGaussLobattoLegendre_weights_and_roots(mporder,w1d,xi)
 
-         inode = 0
-         do k = 1,mporder+1
-            do i = 1,mporder+1
-               do j = 1,mporder+1
-                  inode = inode + 1
-                  xgp(atoIJK(inode),1:3) = [xi(lorder(i)), xi(lorder(j)), xi(lorder(k))]
-                  wgp(atoIJK(inode)) = w1d(lorder(i))*w1d(lorder(j))*w1d(lorder(k))
-               end do
-            end do
-         end do
+			lorder(1) = 1
+			lorder(2) = mporder+1
+			do i = 3,mporder+1
+			lorder(i) = i-1
+			end do
 
-      end subroutine GaussLobattoLegendre_hex
+			inode = 0
+			do k = 1,mporder+1
+				do i = 1,mporder+1
+					do j = 1,mporder+1
+						inode = inode + 1
+						xgp(atoIJK(inode),1:3) = [xi(lorder(i)), xi(lorder(j)), xi(lorder(k))]
+						wgp(atoIJK(inode)) = w1d(lorder(i))*w1d(lorder(j))*w1d(lorder(k))
+					end do
+				end do
+			end do
 
-      subroutine getGaussPoints_equispaced_hex(mporder,mngaus,atoIJK,xgp)
-         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-         ! Closed rule quadrature that uses the     !
-         ! Chebyshev grid+enddpoints as abcissas.   !
-         ! Weights are obtained by evaluating       !
-         ! w_j = int(l^n_i(xi_j),-1,1).             !
-         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-         implicit none
-         integer(4),intent(in) :: mporder,mngaus,atoIJK(mngaus)
-         real(rp),intent(out)  :: xgp(mngaus,ndime)
-         integer(4)            :: inode,i,j,k,lorder(mporder+1)
-         !real(rp)              :: xi(mporder+1),w1d(mporder+1),w0,w1,w2,w3
-         real(rp)              :: xi_equi(mporder+1)
+		end subroutine GaussLobattoLegendre_hex
 
-         call getEquispaced_roots(mporder,xi_equi)
+		subroutine getGaussPoints_equispaced_hex(mporder,mngaus,atoIJK,xgp)
+
+			implicit none
+			integer(4),intent(in) :: mporder,mngaus,atoIJK(mngaus)
+			real(rp),intent(out)  :: xgp(mngaus,ndime)
+			integer(4)            :: inode,i,j,k,lorder(mporder+1)
+			real(rp)              :: xi_equi(mporder+1)
+
+			call getEquispaced_roots(mporder,xi_equi)
 
 			!write(*,*) 'xi_equi',xi_equi
 
-         lorder(1) = 1
-         lorder(2) = mporder+1
-         do i = 3,mporder+1
-            lorder(i) = i-1
-         end do
+			lorder(1) = 1
+			lorder(2) = mporder+1
+			do i = 3,mporder+1
+			lorder(i) = i-1
+			end do
 
-         inode = 0
-         do k = 1,mporder+1
-            do i = 1,mporder+1
-               do j = 1,mporder+1
-                  inode = inode + 1
-                  xgp(atoIJK(inode),1:3) = [xi_equi(lorder(i)), xi_equi(lorder(j)), xi_equi(lorder(k))]
-               end do
-            end do
-         end do
+			inode = 0
+			do k = 1,mporder+1
+				do i = 1,mporder+1
+					do j = 1,mporder+1
+						inode = inode + 1
+						xgp(atoIJK(inode),1:3) = [xi_equi(lorder(i)), xi_equi(lorder(j)), xi_equi(lorder(k))]
+					end do
+				end do
+			end do
 
 			!write(*,*) 'xgp',xgp(:,:)
 
-      end subroutine getGaussPoints_equispaced_hex
-
-
+		end subroutine getGaussPoints_equispaced_hex
 
 end module quadrature_rules
