cbd8166e5762d3c0e71f119c154c258c1b80f2d9
bef-18 <beneteiximenofranch@hotmail.com>
2023-10-17 17:04:52 +0200

    fix quality computation

--- src/lib_sod2d/sources/mod_mesh_quality.f90
+++ src/lib_sod2d/sources/mod_mesh_quality.f90
@@ -19,12 +19,12 @@ contains
 		real(rp),   intent(in)  :: elemJ(ndime,ndime), idealJ(ndime,ndime)
 		real(rp),   intent(out) :: eta
 		real(rp)                :: S(ndime,ndime), S2(ndime,ndime), sigma, Sf, detS
-        integer(4)              :: d=3
+        real(rp)             :: d=3.0_rp
         S     = matmul(elemJ, idealJ)
         detS  = (S(1,1)*S(2,2)*S(3,3)+S(1,2)*S(2,3)*S(3,1)+S(1,3)*S(2,1)*S(3,2)-S(3,1)*S(2,2)*S(1,3)-S(3,2)*S(2,3)*S(1,1)-S(3,3)*S(2,1)*S(1,2))
         sigma = (detS + abs(detS))/2
         S2    = matmul(transpose(S), S)
         Sf    = S2(1,1) + S2(2,2) + S2(3,3)
-        eta   = Sf/(d*sigma**(d/2)) !! Check value of d with Abel
+        eta   = Sf/(d*sigma**(2.0_rp/d)) 
     end subroutine
 end module
