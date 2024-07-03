3d772b290f22f29f8c7228f3794e7679322161bb
Arnau Miro <arnau.miro@upc.edu>
2024-07-03 12:26:47 +0200

    WIP: adding self detection of CUDA version using nvcc

--- cmake/nvidia-smi-cc.cmake
+++ cmake/nvidia-smi-cc.cmake
@@ -12,3 +12,19 @@ function(set_cc)
 	message("GPU compute capability detected: cc" ${GPU_CC})
 	set(GPU_CC ${GPU_CC} PARENT_SCOPE)
 endfunction()
+
+# Use NVCC to automatically find the
+# CUDA version
+function(set_cuda)
+	# Obtain the git hash and store it to a variable
+	execute_process(
+		COMMAND nvcc --version
+		COMMAND grep release
+		COMMAND cut -d ' ' -f 5
+		COMMAND cut -d ',' -f 1
+		OUTPUT_VARIABLE GPU_CUDA
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	message("CUDA version detected: " ${GPU_CUDA})
+	set(GPU_CUDA ${GPU_CUDA} PARENT_SCOPE)
+endfunction()
