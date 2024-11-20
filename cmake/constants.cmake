eb29b96a07fd42c34b69ab0a29a88a69db978492
Arnau Miro <arnau.miro@upc.edu>
2024-11-20 13:05:06 +0100

    Adding porder, rp, rp_vtk and rp_avg to be set by cmake

---
+++ cmake/constants.cmake
@@ -0,0 +1,13 @@
+message("-- SOD2D constants configuration:")
+
+# Give some user info
+message("   -- Using porder: " ${USE_PORDER})
+message("   -- Using real precision: " ${USE_RP})
+message("   -- Using vtk precision: " ${USE_RP_VTK})
+message("   -- Using average precision: " ${USE_RP_AVG})
+
+# Add definitions for porder and precision
+add_definitions(-D__PORDER__=${USE_PORDER})
+add_definitions(-D__RP__=${USE_RP})
+add_definitions(-D__RP_VTK__=${USE_RP_VTK})
+add_definitions(-D__RP_AVG__=${USE_RP_AVG})
